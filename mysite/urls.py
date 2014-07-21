755f53bae054869d799f96315fa6a22cb523a21b
Benjamin Suire <bensuire@gmail.com>
2014-07-21 21:39:09 +0200

    Prject files

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('photos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
