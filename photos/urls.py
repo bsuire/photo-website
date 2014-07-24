7a93309d17d72990e3ee9804f4ab533aecfd03dc
Benjamin Suire <bensuire@gmail.com>
2014-07-24 05:26:02 +0200

    Added gallery page with lightbox. Bootstrap is now sourced.

--- photos/urls.py
+++ photos/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from photos import views
 
 urlpatterns = [
-            url(r'^$', views.home, name='home')
+            url(r'^$', views.home, name='home'),
+            url(r'^aboutme/$', views.aboutme, name='aboutme'),
+            url(r'^(?P<album_name>\w+)/$', views.gallery, name='gallery'),
+            
             ]
--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+existing_albums=["madagascar"]
+
 def home(request):
     context = {}
     return render(request,'photos/home.html',context)
 
+def aboutme(request):
+    context = {}
+    return render(request,'photos/aboutme.html',context)
 
-# Create your views here.
+def gallery(request,album_name):
+    context = {'album_name':album_name}
+    if (existing_albums.count(album_name) == 0):
+        return render(request,'photos/error404.html',context)
+    else:
+        return render(request,'photos/'+album_name+'.html',context)
+
+def pageNotFound(request,album_name):
+    context = {'album_name':album_name}
+    return render(request,'photos/error404.html',context)
